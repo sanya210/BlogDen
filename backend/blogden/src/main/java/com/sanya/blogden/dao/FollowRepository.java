657aa6a22fd65a64c22ac99ef2e1391effcf7d03
Sanya Srivastava <sanyasrivastava2@gmail.com>
2023-05-18 16:06:47 +0530

    implemented follow, comment, delete followers

--- backend/blogden/src/main/java/com/sanya/blogden/dao/FollowRepository.java
+++ backend/blogden/src/main/java/com/sanya/blogden/dao/FollowRepository.java
@@ -1,7 +1,9 @@
 package com.sanya.blogden.dao;
 
 import com.sanya.blogden.entity.Follow;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -13,4 +15,9 @@ public interface FollowRepository extends JpaRepository<Follow, Integer> {
 
     @Query(value = "select followee_id from follow where follower_id=:id",nativeQuery = true)
     public List<Integer> following(int id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from follow where follower_id=:follower_id and followee_id=:followee_id", nativeQuery = true)
+    public void deleteFollow(int follower_id, int followee_id);
 }
--- backend/blogden/src/main/java/com/sanya/blogden/rest/FollowRESTController.java
+++ backend/blogden/src/main/java/com/sanya/blogden/rest/FollowRESTController.java
@@ -1,5 +1,6 @@
 package com.sanya.blogden.rest;
 
+import com.sanya.blogden.dao.FollowRepository;
 import com.sanya.blogden.dao.UserRepository;
 import com.sanya.blogden.entity.Comments;
 import com.sanya.blogden.entity.Follow;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/follow")
@@ -27,14 +29,21 @@ public class FollowRESTController {
     @Autowired
     private UserRepository userRepo;
 
+    @Autowired
+    private FollowRepository followRepository;
+
 //    @Autowired
 //    public FollowRESTController(FollowService followService) {
 //        this.followService = followService;
 //    }
-    @PostMapping("/save")
-    public ResponseEntity<Follow> addFollow(@RequestBody Follow follow){
+    @GetMapping("/save")
+    public ResponseEntity<Follow> addFollow(@RequestParam int followerId,@RequestParam int followeeId){
+        Follow follow = new Follow();
+        Optional<User> user = userRepo.findById(followerId);
         follow.setFollowId(0);
-
+        follow.setFollower(user.get());
+        user = userRepo.findById(followeeId);
+        follow.setFollowee(user.get());
         Follow savedFollower = followService.save(follow);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedFollower);
     }
@@ -54,5 +63,9 @@ public class FollowRESTController {
         return userRepo.findAllById(followService.following(id));
     }
 
-
+    @GetMapping("/delete")
+    public ResponseEntity<?> deleteFollow(@RequestParam int followerId,@RequestParam int followeeId){
+        followRepository.deleteFollow(followerId,followeeId);
+        return ResponseEntity.ok("Deleted");
+    }
 }
